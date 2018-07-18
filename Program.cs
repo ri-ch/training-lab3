1045290c07d9a87ee852de2d1b2ff79ca283a0fe
unknown <rich.crawley@gmail.com>
2018-07-18 15:43:14 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace DynamoLab
+{
+    class Program
+    {
+        private static IConfiguration _config;
+        static void Main(string[] args)
+        {
+             _config = 
+                new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
